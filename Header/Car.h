9bd4bf5a0b771a70568a239caf61639798ef2571
Arman Ebrahimi Khoshdel <armanebrahimikhoshdel@gmail.com>
2026-09-24 17:49:09 +0330

    arcade drifting

--- Header/Car.h
+++ Header/Car.h
@@ -20,7 +20,7 @@ class Car {
     public:
         const float max_long_force =45;
         const float acceleration = 20;
-        const float deceleration = 20;
+        const float deceleration = 15;
         const float max_wheel_rotation = 50;
 
         Car();
